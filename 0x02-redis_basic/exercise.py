ae5d792942629441a613629e79ea6e757f42b4d2
Alpha Infinos <kevinmullerhi@gmail.com>
2025-07-09 20:10:39 +0300

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,37 +1,21 @@
 #!/usr/bin/env python3
 """
-Module cache
-Defines a Cache class for storing data in Redis
+Test the Cache class
 """
 
-import redis
-import uuid
-from typing import Union
+from cache import Cache
 
+cache = Cache()
 
-class Cache:
-    """
-    Cache class that provides methods to store data in Redis
-    """
+# Test storing different data types
+key1 = cache.store("Hello Redis!")
+key2 = cache.store(123)
+key3 = cache.store(45.67)
+key4 = cache.store(b"bytes data")
 
-    def __init__(self):
-        """
-        Initialize a Redis client and flush the database
-        """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store the given data in Redis with a random key
-
-        Args:
-            data (Union[str, bytes, int, float]): Data to store
-
-        Returns:
-            str: The key under which the data is stored
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+print("Stored keys:")
+print(key1)
+print(key2)
+print(key3)
+print(key4)
 
